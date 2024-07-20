9490cb862978e1d8e836407f68bc2edcb24e8502
abpa123 <warrenlunch@gmail.com>
2024-07-20 12:08:51 -0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       adityabanwasi                                                #
+# 	Created:      7/20/2024, 12:08:01 PM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
